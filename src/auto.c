bcc3da2da6c7c22b17acecb1613e52a845af0226
Ethan Gordon <egordon9dev@gmail.com>
2017-10-24 16:26:15 -0500

    back to commit 95afcb9eb4b216e0b8bdd7f0591356fe419b37b6

--- src/auto.c
+++ src/auto.c
@@ -2,12 +2,10 @@
 #include "pid.h"
 #include "setup.h"
 
-int autonDrive(double dist, PidVars *left, PidVars *right, bool turning)
-{
+int autonDrive(double dist, PidVars* left, PidVars* right, bool turning) {
 	pidDrive(dist, left, right, turning);
 	int wait = 200;
-	if (left->doneTime + wait < millis() && right->doneTime + wait < millis())
-	{
+	if(left->doneTime + wait < millis() && right->doneTime + wait < millis()) {
 		left->doneTime = LONG_MAX;
 		right->doneTime = LONG_MAX;
 		return 1;
@@ -15,8 +13,7 @@ int autonDrive(double dist, PidVars *left, PidVars *right, bool turning)
 	return 0;
 }
 
-void auton1(PidVars *DL_pid, PidVars *DR_pid, PidVars *DLturn_pid, PidVars *DRturn_pid, PidVars *arm_pid, PidVars *cb_pid)
-{
+void auton1(PidVars* DL_pid, PidVars* DR_pid, PidVars* DLturn_pid, PidVars* DRturn_pid, PidVars* arm_pid, PidVars* cb_pid) {
 	printf("starting auton.....");
 	unsigned long t0 = millis();
 	double cbAngle = 180, armAngle = 75;
@@ -24,120 +21,53 @@ void auton1(PidVars *DL_pid, PidVars *DR_pid, PidVars *DLturn_pid, PidVars *DRtu
 	resetMotors();
 	resetDrive(DL_pid, DR_pid, DLturn_pid, DRturn_pid);
 	t0 = millis();
-	while (true)
-	{
+	while (true) {
 		pidArm(arm_pid, armAngle);
 		pidCB(cb_pid, cbAngle);
-		switch (step)
-		{
-		case 0:
-			setClaw(-127);
-			if (millis() - t0 > 300)
-			{
-				setClaw(-30);
-			}
-			cbAngle = 130;
-			armAngle = 75;
-			step += autonDrive(-68, DL_pid, DR_pid, false);
-			if (step == 1)
-			{
-				resetDrive(DL_pid, DR_pid, DLturn_pid, DRturn_pid);
-				setDL(0);
-				setDR(0);
-				printf("\n\nstep: 1\n\n");
-			}
-			break;
-		case 1:
-			step += autonDrive(10, DLturn_pid, DRturn_pid, true);
-			if (step == 2)
-			{
-				resetDrive(DL_pid, DR_pid, DLturn_pid, DRturn_pid);
-				setDL(0);
-				setDR(0);
-				printf("\n\nstep: 2\n\n");
-				t0 = millis();
-			}
-			break;
-		case 2:
-			setMGL(-127);
-			if (millis() - t0 > 2500)
-			{
-				stack(arm_pid, cb_pid, 1);
-			}
-			if (millis() - t0 > 2700)
-			{
-				setClaw(25);
-			}
-			if (!digitalRead(MGL_LIM))
-			{
-				step += autonDrive(48, DL_pid, DR_pid, false);
-			}
-
-			if (step == 3)
-			{
-				resetDrive(DL_pid, DR_pid, DLturn_pid, DRturn_pid);
-				setDL(0);
-				setDR(0);
-				printf("\n\nstep: 3\n\n");
-			}
-			break;
-		case 3:
-			setMGL(-127);
-			if (millis() - t0 > 2500)
-			{
-				stack(arm_pid, cb_pid, 1);
-			}
-			if (millis() - t0 > 2700)
-			{
-				setClaw(25);
-			}
-			step += autonDrive(160, DLturn_pid, DRturn_pid, true);
-			if (step == 4)
-			{
-				resetDrive(DL_pid, DR_pid, DLturn_pid, DRturn_pid);
-				t0 = millis();
-				printf("\n\nstep: 4\n\n");
-			}
-			break;
-		case 4:
-			if (millis() - t0 < 3500)
-			{
-				setDL(-127);
-				setDR(-127);
-			}
-			else
-			{
-				setDL(0);
-				setDR(0);
-				step++;
-				t0 = millis();
-				printf("\n\nstep 5:\n\n");
-			}
-			break;
-		case 5:
-			if (millis() - t0 < 750)
-			{
-				setMGL(127);
-			}
-			else
-			{
-				step++;
-				t0 = millis();
-				printf("\n\nstep 6:\n\n");
-			}
-			break;
-		case 6:
-			//-----------------------------------------------------------------------------------------------------------
-			//									SPIN-CYCLE
-			//-----------------------------------------------------------------------------------------------------------
-			if (((millis() - t0) / 200) % 2 == 1)
-			{
-				setMGL(127);
-			}
-			else
-			{
+		switch(step) {
+			case 0:
+				setClaw(-127);
+				if(millis() - t0 > 300) {
+					setClaw(-30);
+				}
+				cbAngle = 130;
+				armAngle = 75;
+				step += autonDrive(-68, DL_pid, DR_pid, false);
+				if(step == 1) {
+					resetDrive(DL_pid, DR_pid, DLturn_pid, DRturn_pid);
+					setDL(0);setDR(0);
+					printf("\n\nstep: 1\n\n");
+				}
+				break;
+			case 1:
+				step += autonDrive(10, DLturn_pid, DRturn_pid, true);
+				if(step == 2) {
+					resetDrive(DL_pid, DR_pid, DLturn_pid, DRturn_pid);
+					setDL(0);setDR(0);
+					printf("\n\nstep: 2\n\n");
+					t0 = millis();
+				}
+				break;
+			case 2:
 				setMGL(-127);
-<<<<<<< HEAD
+				if(millis() - t0 > 2500) {
+					stack(arm_pid, cb_pid, 1);
+				}
+				if(millis() - t0 > 2700) {
+					setClaw(25);
+				}
+				if(!digitalRead(MGL_LIM)) {
+					step += autonDrive(48, DL_pid, DR_pid, false);
+				}
+
+				if(step == 3) {
+					resetDrive(DL_pid, DR_pid, DLturn_pid, DRturn_pid);
+					setDL(0);setDR(0);
+					printf("\n\nstep: 3\n\n");
+				}
+				break;
+			case 3:
+				setMGL(-127);
 				if(millis() - t0 > 2500) {
 					stack(arm_pid, cb_pid, 1);
 				}
@@ -201,38 +131,9 @@ void auton1(PidVars *DL_pid, PidVars *DR_pid, PidVars *DLturn_pid, PidVars *DRtu
 					printf("\n\nstep 7:\n\n");
 				}
 				break;
-=======
-			}
->>>>>>> 81cf8fbdb018f61cee6fb786127c6f129a154324
 
-			if (millis() - t0 < 1300)
-			{
-				setDL(127);
-				setDR(127);
-			}
-			else
-			{
-				step++;
-				setDL(0);
-				setDR(0);
-				t0 = millis();
-				printf("\n\nstep 7:\n\n");
-			}
-			break;
-		case 7:
-			if (digitalRead(MGL_LIM))
-			{
-				setMGL(-127);
-			}
-			else
-			{
-				step++;
-				t0 = millis();
-				printf("\n\nstep 7:\n\n");
-			}
-			break;
 
-			/*
+				/*
 			case 4:
 				setDR(-127);
 				setDL(-127);
@@ -278,20 +179,6 @@ void auton1(PidVars *DL_pid, PidVars *DR_pid, PidVars *DLturn_pid, PidVars *DRtu
  * The autonomous task may exit, unlike operatorControl() which should never exit. If it does
  * so, the robot will await a switch to another mode or disable/enable cycle.
  */
-void autonomous()
-{
-	switch (autonSelect)
-	{
-	case 1:
-		auton1(&DL_pid, &DR_pid, &DLturn_pid, &DRturn_pid, &arm_pid, &cb_pid);
-		break;
-
-	case 2:
-		//autonSkills();
-		break;
-
-	case 3:
-		return;
-		break;
-	}
+void autonomous() {
+	auton1(&DL_pid, &DR_pid, &DLturn_pid, &DRturn_pid, &arm_pid, &cb_pid);
 }
--- src/init.c
+++ src/init.c
@@ -6,124 +6,9 @@
  * states (digitalWrite()) of limit switches, push buttons, and solenoids. It can also safely
  * configure a UART port (usartOpen()) but cannot set up an LCD (lcdInit()).
  */
-void initializeIO()
-{
-    pinMode(MGL_LIM, INPUT);
+void initializeIO() {
+     pinMode(MGL_LIM, INPUT);
 }
-
-#define kButtonNone 0
-#define kButtonLeft LCD_BTN_LEFT
-#define kButtonCenter LCD_BTN_CENTER
-#define kButtonRight LCD_BTN_RIGHT
-
-// default auton
-//autonSelect = 1;
-
-// Little macro to keep code cleaner, masks both disable/enable and auton/driver
-#define vexCompetitionState ((isEnabled() ? 0 : 2) + (isAutonomous() ? 1 : 0))
-
-int getLcdButtons()
-{
-    int competitionState = vexCompetitionState;
-    int buttons;
-
-    // This function will block until either
-    // 1. A button is pressed on the LCD
-    //    If a button is pressed when the function starts then that button
-    //    must be released before a new button is detected.
-    // 2. Robot competition state changes
-
-    // Wait for all buttons to be released
-    while (lcdReadButtons(uart1) != kButtonNone)
-    {
-        // check competition state, bail if it changes
-        if (vexCompetitionState != competitionState)
-            return (kButtonNone);
-        taskDelay(10);
-    }
-
-    // block until an LCD button is pressed
-    do
-    {
-        // we use a copy of the lcd buttons to avoid their state changing
-        // between the test and returning the status
-        buttons = lcdReadButtons(uart1);
-
-        // check competition state, bail if it changes
-        if (vexCompetitionState != competitionState)
-            return (kButtonNone);
-
-        taskDelay(10);
-    } while (buttons == kButtonNone);
-
-    return (buttons);
-}
-
-/*-----------------------------------------------------------------------------*/
-/*  Display autonomous selection                                               */
-/*-----------------------------------------------------------------------------*/
-void LcdSetAutonomous(int value)
-{
-    if (value == 1)
-    {
-        lcdSetText(uart1, 1, "Match");
-        lcdSetText(uart1, 2, "[1]     2     X ");
-    }
-    if (value == 2)
-    {
-        lcdSetText(uart1, 1, "Skills");
-        lcdSetText(uart1, 2, " 1     [2]    X ");
-    }
-    if (value == 3)
-    {
-        lcdSetText(uart1, 1, "NOTHING");
-        lcdSetText(uart1, 2, " 1      2    [X]");
-    }
-
-    // Save autonomous mode for later
-    autonSelect = value;
-}
-
-/*-----------------------------------------------------------------------------*/
-/*  Select one of three autonomous choices                                     */
-/*-----------------------------------------------------------------------------*/
-
-void LcdAutonomousSelection()
-{
-    int button;
-
-    // Clear LCD and turn on backlight
-    lcdClear(uart1);
-    lcdSetBacklight(uart1, true);
-
-    // default choice
-    LcdSetAutonomous(1);
-    // PROS seems to need a delay
-    taskDelay(2000);
-
-    while (!isEnabled())
-    {
-        // this function blocks until button is pressed
-        button = getLcdButtons();
-
-        // Display and select the autonomous routine
-        if (button == kButtonLeft)
-            LcdSetAutonomous(1);
-
-        if (button == kButtonCenter)
-            LcdSetAutonomous(2);
-
-        if (button == kButtonRight)
-            LcdSetAutonomous(3);
-
-        // Don't hog the cpu !
-        taskDelay(20);
-    }
-
-    lcdSetText(uart1, 1, "PID PID MOOSE");
-    lcdSetText(uart1, 2, "DUCK DUCK GOOSE");
-}
-
 /*
  * Runs user initialization code. This function will be started in its own task with the default
  * priority and stack size once when the robot is starting up. It is possible that the VEXnet
@@ -137,10 +22,6 @@ void LcdAutonomousSelection()
  * will not start. An autonomous mode selection menu like the pre_auton() in other environments
  * can be implemented in this task if desired.
  */
-void initialize()
-{
-    setupEnc();
-
-    lcdInit(uart1);
-    LcdAutonomousSelection();
+void initialize() {
+     setupEnc();
 }
